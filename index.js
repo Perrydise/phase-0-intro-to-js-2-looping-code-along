3dad604dc710cdce1f6970113d659049c4641e54
Perry Jorss <pjorss123@gmail.com>
2022-04-18 17:39:03 -0400

    commit tree 0b880d9b7417e85f0f91f474594cd90e11368394

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(cards, eventName){
+    let thankCard = []
+    for (let i = 0; i < cards.length; i++) {
+        thankCard.push(`Thank you, ${cards[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankCard;
+}
+
+function countDown(number){
+let i = 0
+while (i > 0) {
+    console.log(number);
+    i--
+}
+return number
+}
